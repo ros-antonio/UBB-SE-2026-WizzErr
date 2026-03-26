773befbbd40e15fdda33637a24a0df35ec86d275
Pop Stelian <122468830+PopStelian@users.noreply.github.com>
2026-03-26 12:47:40 +0200

    Modify Tickets table structure and constraints
    Updated the Tickets table definition to include a NOT NULL constraint on flight_id and added a comment regarding the foreign key.

--- Database/02_Tickets.sql
+++ Database/02_Tickets.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Tickets (
-    ticket_id INT PRIMARY KEY IDENTITY(1,1),
-    user_id INT FOREIGN KEY REFERENCES Users(user_id),
-    flight_id INT FOREIGN KEY REFERENCES Flights(flight_id),
-    seat NVARCHAR(10),
-    price DECIMAL(10, 2) NOT NULL,
-    status NVARCHAR(20) DEFAULT 'Active',
-    passenger_first_name NVARCHAR(50) NOT NULL,
-    passenger_last_name NVARCHAR(50) NOT NULL,
-    passenger_email NVARCHAR(100),
-    passenger_phone NVARCHAR(20),
+ticket_id INT PRIMARY KEY IDENTITY(1,1),
+user_id INT FOREIGN KEY REFERENCES Users(user_id),
+flight_id INT NOT NULL, --Foreign Key will be added after Paul's team provides us the Flight script
+seat NVARCHAR(10),
+price DECIMAL(10, 2) NOT NULL,
+status NVARCHAR(20) DEFAULT 'Active',
+passenger_first_name NVARCHAR(50) NOT NULL,
+passenger_last_name NVARCHAR(50) NOT NULL,
+passenger_email NVARCHAR(100),
+passenger_phone NVARCHAR(20)
 );
