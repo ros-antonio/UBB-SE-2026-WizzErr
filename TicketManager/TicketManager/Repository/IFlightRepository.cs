bd4feb05ee36c7837132a83b3a4950db127481d2
davidcristian11 <strimbu.david11@gmail.com>
2026-03-30 12:59:50 +0300

    Created repository interfaces

---
+++ TicketManager/TicketManager/Repository/IFlightRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using TicketManager.Domain;
+
+namespace TicketManager.Domain.Repositories
+{
+    public interface IFlightRepository
+    {
+        IEnumerable<Flight> GetFlightsByRoute(string departure, string destination, DateTime date);
+        Flight GetFlightById(int id);
+    }
+}
---
+++ TicketManager/TicketManager/Repository/IMembershipRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TicketManager.Domain;
+
+namespace TicketManager.Domain.Repositories
+{
+    public interface IMembershipRepository
+    {
+        Membership GetMembershipById(int id);
+        IEnumerable<MembershipAddonDiscount> GetAddonDiscounts(int membershipId);
+    }
+}
